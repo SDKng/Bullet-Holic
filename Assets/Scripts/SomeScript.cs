81eeb9a37b158047cd3a7255c88f5892e9ee84d1
MatthewFerreira2020 <38076237+MatthewFerreira2020@users.noreply.github.com>
2020-02-05 14:26:27 -0500

    .

---
+++ Assets/Scripts/SomeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+    
+
+class SomeScript : MonoBehaviour
+{
+    private const string pluginName = "BULLETHOLICPHYSICS.dll";
+    [DllImport(pluginName)]
+
+
+    private static extern float FooPluginFunction();
+
+    void Awake()
+    {
+        // Calls the FooPluginFunction inside the plugin
+        // And prints 5 to the console
+        Debug.Log(FooPluginFunction());
+    }
+}
