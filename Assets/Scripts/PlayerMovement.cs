ae8095628e582cd8a7caa0af48e9b7c450bdb454
SDKng <Jacobalexandersaur@gmail.com>
2020-02-06 20:12:27 -0500

    Starting Unity Scripts

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 Direction;
+    GameObject Player;
+
+    void Start()
+    {
+        Direction = new Vector2();
+        //Player = GameObject.FindObjectWithTag("Player");
+
+    }
+
+
+    void UpdateDirection(Vector2 axisData)
+    {
+        Direction = Direction + axisData;
+
+    }
+
+    void PlayerMove(bool canMove)
+    {
+        Debug.Log("Direction");
+
+
+
+
+        Direction = new Vector2();
+    }
+}
---
+++ NativePlugins/Bullet Holic Physics/Bullet Holic Physics/Bullet Holic Physics.cpp
@@ -0,0 +1,14 @@
+// Bullet Holic Physics.cpp : Defines the exported functions for the DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Bullet Holic Physics.h"
+
+#define FOOPLUGINFUNCTION_API __declspec(dllexport)
+
+extern "C" {
+	FOOPLUGINFUNCTION_API float FooPluginFunction() { return 5.0F; }
+}
+
+
